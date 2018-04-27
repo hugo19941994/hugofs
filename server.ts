76a667e84b8dc8db5591735a28995e77280e0cca
Hugo Ferrando Seage <hugoseage@gmail.com>
2018-04-27 23:05:00 +0200

    Add compression middleware

--- server.ts
+++ server.ts
@@ -7,11 +7,15 @@ import * as express from 'express';
 import { join } from 'path';
 import { readFileSync } from 'fs';
 
+const compression = require('compression')
+
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
 
 // Express server
 const app = express();
+
+app.use(compression());
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
