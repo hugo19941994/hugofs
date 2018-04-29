70dd262270daae3a25f3d127797091e69e2040a9
Hugo Ferrando Seage <hugoseage@gmail.com>
2018-04-29 20:43:56 +0200

    Refactored backend
    Now compressed image thumbnails using webp if client advertises support

--- backend.js
+++ backend.js
@@ -1,83 +1,114 @@
-var {Router} = require('express');
-var Promise = require("bluebird");
-var fs = Promise.promisifyAll(require('fs'));
-var marked = require('marked');
-var yaml = require('js-yaml');
-var gm = require('gm');
-var express = require('express');
-var bodyParser = require('body-parser');
-var compression = require('compression');
-Promise.promisifyAll(gm.prototype);
+const compression = require('compression');
+const express = require('express');
+const fs = require('fs');
+const marked = require('marked');
+const sharp = require('sharp');
+const util = require('util');
+const yaml = require('js-yaml');
 
-function photosApi() {
-  var router = Router()
+const readdir = util.promisify(fs.readdir);
+const readFile = util.promisify(fs.readFile);
 
-  let images = []
+let images = []
 
-  fs.readdirAsync(__dirname + '/photos/')
-  .each((folder) => {
-    if (folder !== ".gitignore") {
-      return fs.readdirAsync(__dirname + '/photos/' + folder).each((file) => {
-        images.push(__dirname + '/photos/' + folder + '/' + file)
-      })
+async function prepare_photos() {
+  const dir = `${__dirname}/photos/`
+  const folders = await readdir(dir);
+
+  for (let folder of folders) {
+    if (folder === '.gitignore') {
+      continue;
     }
-  })
 
-  router.route('/photos')
-  .get(function(req, res) {
-    res.json(images.length)
-  })
-
-  router.route('/photo/:id')
-  .get(function (req, res) {
-    if (parseInt(req.params.id) > images.length){
-      res.sendStatus(500)
+    let files = await readdir(`${dir}${folder}`);
+    for (let file of files) {
+      images.push(`${dir}${folder}/${file}`);
     }
-    else {
-      return gm(images[req.params.id]).autoOrient().resize(500).toBufferAsync('jpg').then((data) => {
-        res.json({photo: data.toString('base64'), num: req.params.id})
-      })
-    }
-  })
-
-  router.route('/bigphoto/:id')
-  .get(function (req, res) {
-    res.sendFile(images[req.params.id])
-  })
-
-  return router;
+  }
 }
 
-function postsApi() {
-  var router = Router()
+// Load photo routes (TODO: load photos directly)
+try {
+  prepare_photos();
+} catch (err) {
+  console.log(err)
+}
 
-  marked.setOptions({
-    highlight: function(code) {
-      return '<pre class="hljs"><code>' +
-               require('highlight.js').highlightAuto(code).value +
-               '</code></pre>';
+// Highlighting options
+marked.setOptions({
+  highlight: function(code) {
+    return '<pre class="hljs"><code>' +
+      require('highlight.js').highlightAuto(code).value +
+      '</code></pre>';
+  }
+});
+
+const app = express();
+
+// Port
+app.set('port', process.env.PORT || 3060);
+
+// Middleware
+app.use(compression());
+
+// Endpoints
+app.get('/photosApi/photos', (req, res, next) => {
+  res.json(images.length);
+});
+
+app.get('/photosApi/photo/:id', (req, res, next) => {
+  if (parseInt(req.params.id) > images.length) {
+    res.sendStatus(500);
+  }
+  else {
+    // Checks for webp support
+    if (req.get('accept').indexOf('image/webp') > -1) {
+      return sharp(images[req.params.id]).resize(500).webp().toBuffer().then(data => {
+        res.json({photo: data.toString('base64'), num: req.params.id})
+      });
+    } else {
+      return sharp(images[req.params.id]).resize(500).jpeg({progressive: true}).toBuffer().then(data => {
+        res.json({photo: data.toString('base64'), num: req.params.id})
+      });
     }
-  });
+  }
+});
 
-  router.route('/post/:post_id')
-  .get(function (req, res) {
-    return fs.readdirAsync('./posts/')
-    .then((files) => {
-      files.forEach((file) => {
-        return fs.readFileAsync('./posts/' + file)
-        .then((a) => {
-          if (yaml.load(a).title == req.params.post_id.replace(/_/g, ' ')) {
-            let r = yaml.load(a)
-            marked(r.text, function (err, content) {
-              r.text = content;
-                //console.log(err)
-              res.json(r)
-            })
-          }
-        })
-      })
-    })
-  })
+app.get('/photosApi/bigphoto/:id', (req, res, next) => {
+  res.sendFile(images[req.params.id]);
+});
+
+// TODO: Load posts and highlight beforehand
+app.get('/api/post/:post_id', async (req, res, next) => {
+  const posts = await readdir('./posts');
+  let found = false;
+
+  for (let file of posts) {
+    const post = await readFile(`./posts/${file}`);
+    let r = yaml.load(post);
+
+    if (r.title === req.params.post_id.replace(/_/g, ' ')) {
+      marked(r.text, (err, content) => {
+        found = true;
+        r.text = content;
+        return res.json(r);
+      });
+    }
+  }
+
+  if (!found) res.sendStatus(400);
+});
+
+app.get('/api/posts', async (req, res, next) => {
+  let r = [];
+
+  const posts = await readdir('./posts');
+
+  for (let file of posts) {
+    const post = await readFile(`./posts/${file}`);
+    let doc = yaml.load(post);
+    r.push({"title": doc.title, "date": doc.date});
+  }
 
   function compare(a, b) {
     if (a.date < b.date)
@@ -87,36 +118,7 @@ function postsApi() {
     return 0;
   }
 
-  router.route('/posts')
-  .get(function(req, res) {
-    let r = []
-    fs.readdirAsync('./posts/')
-    .each((file) => {
-      return fs.readFileAsync('./posts/' + file)
-      .then((a) => {
-        let doc = yaml.load(a)
-        r.push({"title": doc.title, "date": doc.date})
-      })
-    })
-    .then(() => {
-      res.json(r.sort(compare))
-    })
-  })
+  res.json(r.sort(compare));
+})
 
-  return router
-}
-
-const app = express();
-
-// Port
-app.set('port', process.env.PORT || 3060);
-
-// Middleware
-app.use(bodyParser.json());
-app.use(compression());
-
-// Endpoints
-app.use('/api', postsApi());
-app.use('/photosApi', photosApi());
-
-app.listen(app.get('port'))
+app.listen(app.get('port'));
