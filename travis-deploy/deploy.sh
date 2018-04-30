2697a10fe720226ee54bd08f672823d7d318e85e
Hugo Ferrando Seage <hugoseage@gmail.com>
2018-04-30 09:46:15 +0200

    Cleanup old files after deployment and rebuild

--- travis-deploy/deploy.sh
+++ travis-deploy/deploy.sh
@@ -4,6 +4,10 @@ scp -r deploy.zip hfs@hugofs.com:deploy.zip
 ssh hfs@hugofs.com << EOF
   unzip -o deploy.zip -d hugofs
   rm deploy.zip
+  cd hugofs
+  git clean -f -d
+  npm install
+  npm run build:ssr
   systemctl --user restart hugofs.service
   systemctl --user restart hugofsback.service
 EOF
