7af87314b34338c833b474b0e5a455230e673e6d
Hugo Ferrando Seage <hugoseage@gmail.com>
2018-05-02 12:41:47 +0200

    Fix layout when viewing blog posts

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,11 +1,8 @@
 import { Component } from "@angular/core";
-import { ViewService } from "../shared/view.service";
 
 @Component({
   selector: "card",
   styleUrls: ["./card.component.css"],
   templateUrl: "./card.component.html"
 })
-export class CardComponent {
-  constructor(public viewService: ViewService) {}
-}
+export class CardComponent {}
